47842b647380a86cf1f49772b6b593b2eaf73bac
Zafiro2302 <zahiro.garces@hotmail.com>
2024-12-13 14:16:00 -0500

    Initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemigoPreFab;
+    [SerializeField] private TextMeshProUGUI textoOleadas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnearEnemigos());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnearEnemigos()
+    {
+        for (int i = 0; i < 5; i++) //Niveles
+        {
+            for (int j = 0; j < 3; j++) //Oleadas
+            {
+                textoOleadas.text = "Nivel " + (i+1) + " - " + "Oleada " + (j+1);
+                yield return new WaitForSeconds(3f);
+                textoOleadas.text = "";
+                for (int k = 0; k < 10; k++) //Enemigos
+                {
+                    Vector3 puntoAleatorio = new Vector3(transform.position.x, Random.Range(-4.5f, 4.5f), 0);
+                    Instantiate(enemigoPreFab, puntoAleatorio, Quaternion.identity);
+                    yield return new WaitForSeconds(1f);
+                }
+                yield return new WaitForSeconds(2f);
+            }
+            yield return new WaitForSeconds(3f);
+        }
+            
+
+        
+        
+    }
+}
